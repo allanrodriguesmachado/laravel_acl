e532802436f7deae5fea36237f005e3fb37f27b0
Allan Rodrigues <allan.rodrigues14@hotmail.com>
2023-04-08 11:23:42 -0300

    :up:

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::all();
+
+        return view('users.index', [
+            'users' => $users
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        $user = app()->make(User::class);
+        $user->fill([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ])->save();
+
+        return redirect()->route('user.index')->with('success', 'Usuário criado com sucesso!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $user = User::where('id', $id)->first();
+        return view('users.edit', [
+            'user' => $user
+        ]);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return redirect()->route('user.index')->with('error', 'Usuário não encontrado');
+        }
+
+        $user->update([
+            'name' => $request->name,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return redirect()->route('user.index')->with('success', 'Usuário atualizado com sucesso!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        User::destroy($id);
+        return redirect()->route('user.index');
+    }
+}
---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
+
+                    <div class="card-body">
+
+                        <a class="text-success" href="{{route('user.index')}}">&leftarrow; Voltar para a listagem</a>
+
+                        @if($errors)
+                            @foreach($errors->all() as $error)
+                                <div class="alert alert-danger mt-4" role="alert">
+                                    {{ $error }}
+                                </div>
+                            @endforeach
+                        @endif
+
+                        <form action="{{route('user.update', ['user' => $user->id])}}" method="post" class="mt-4" autocomplete="off">
+                            @csrf
+                            @method('PUT')
+                            <div class="form-group">
+                                <label for="name">Nome:</label>
+                                <input type="text" class="form-control" id="name" placeholder="Insira o RECURSO"
+                                       name="name" value="{{ old('name') ?? $user->name}}">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="name">Email:</label>
+                                <input type="text" class="form-control" id="email" placeholder="Insira o RECURSO"
+                                       name="email" value="{{ old('email') ?? $user->email }}">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="name">Senha:</label>
+                                <input type="password" class="form-control" id="password" placeholder="Insira o RECURSO"
+                                       name="password" value="{{ old('password')}}">
+                            </div>
+
+                            <button type="submit" class="mt-2 btn btn-block btn-success">Editar RECURSO</button>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
