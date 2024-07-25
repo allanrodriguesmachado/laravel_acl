abf8976462fbd294b8961007559bbf893ce6a892
ALLAN RODRIGUES MACHADO <38672505883@shibata.corp.br>
2024-07-25 16:09:07 -0300

    Config Router

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,10 +2,10 @@ import {Router, Request, Response} from 'express'
 
 const router = Router();
 
-router.get("/", (request: Request, response: Response) => {
+router.get("/test", (request: Request, response: Response) => {
     return response.json({
-        api: true
-    });
+        name: "Allan Rodrigues"
+    })
 })
 
 export {router};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,25 @@
-import express, {  Request, Response } from "express";
+import express, {  Request, Response, NextFunction } from "express";
+import "express-async-errors";
 import { router } from "./routes";
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json);
+app.use(cors())
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return response.status(404).json({
+            error: err.message
+        })
+    }
+
+    return response.status(500).json({
+        error: 'error',
+        message: 'Internal server error'
+    })
+})
 
 app.listen(3333, () => {console.log("Server ON")});
